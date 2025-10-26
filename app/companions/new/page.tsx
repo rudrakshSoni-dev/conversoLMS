66aadc2c1b596cea9fe770c5ce341a043e7191e1
rudrakshSoni-dev <mrrssoni12@gmail.com>
2025-10-26 18:13:48 +0530

    merge with main

---
+++ app/companions/new/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      New companion form
+    </div>
+  )
+}
+
+export default page
---
+++ app/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+
+    <>
+     sign-in
+    </>
+  )
+}
+
+export default page
