2eebe9ffeca66c79b0efb75119410a487aacf3b6
rudrakshSoni-dev <mrrssoni12@gmail.com>
2025-10-27 20:52:10 +0530

    Database setup

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function page() {
   return (
     <div>
-      id/page 
+      companion created
     </div>
   )
 }
--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,9 +1,14 @@
 
 import CompanionForm from "@/components/CompanionForm"
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-function page() {
+async function page() {
+ const { userId } = await auth();
+ if(!userId) redirect('/sign-in');
+
   return (
-    <main className="min-lg:w-[1/3] min-md:w-2/3 items-center justify-center">
+    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
       <article className="w-full gap-4 flex flex-col">
          <p className="text-2xl font-bold text-center mb-6">Companion Builder</p>
          <CompanionForm />
