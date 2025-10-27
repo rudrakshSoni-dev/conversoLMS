cc68475059e1743226e22b29c7d8c9e702194cab
rudrakshSoni-dev <mrrssoni12@gmail.com>
2025-10-27 18:13:04 +0530

    Added basic billing features

---
+++ app/subscription/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { PricingTable } from '@clerk/nextjs'
+
+function page() {
+  return (
+    <div>
+      Subscription Page
+      <PricingTable />
+    </div>
+  )
+}
+
+export default page
