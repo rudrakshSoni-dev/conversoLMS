f53715318b02af7df958730de3566845f730c543
rudrakshSoni-dev <mrrssoni12@gmail.com>
2025-10-28 13:40:52 +0530

    added something to yaml

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -91,3 +91,17 @@ export const getUserCompanions = async (userId: string,limit=10) => {
 
     return data;
 }
+
+export const getUserSessions = async (userId: string, limit = 10) => {
+    const supabase = createSupabaseClient();
+    const { data, error } = await supabase
+        .from('session_history')
+        .select(`companions:companion_id (*)`)
+        .eq('user_id', userId)
+        .order('created_at', { ascending: false })
+        .limit(limit)
+
+    if(error) throw new Error(error.message);
+
+    return data.map(({ companions }) => companions);
+}
